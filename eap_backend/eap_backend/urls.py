f38c14cb4e3b2f914c9ae6df4e71cd32bed81a79
nbarlowATI <nbarlow@turing.ac.uk>
2021-10-22 18:38:02 +0100

    add app called 'api'

--- eap_backend/eap_backend/urls.py
+++ eap_backend/eap_backend/urls.py
@@ -1,3 +1,5 @@
+
+
 """eap_backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -14,8 +16,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path("api/", include("api.urls")),
     path('admin/', admin.site.urls),
 ]
