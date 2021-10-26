3e796cde39b6e78faaf674f4df9ef103025341be
Flora Roumpani <froumpani@turing.ac.uk>
2021-10-26 13:33:24 +0100

    Create first test queries

--- eap_backend/api/models.py
+++ eap_backend/api/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
 
@@ -6,8 +8,11 @@ class AssuranceCase(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=1000)
     shape = models.IntegerField(default=0)
+    created_date = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.name
+    def was_published_recently(self):
+        return self.created_date >= timezone.now() - datetime.timedelta(days=1)
 
 class TopLevelNormativeGoal(models.Model):
     name = models.CharField(max_length=200)
@@ -16,12 +21,16 @@ class TopLevelNormativeGoal(models.Model):
     keywords = models.CharField(max_length=3000)
     shape = models.IntegerField(default=0)
     assurance_case = models.ForeignKey(AssuranceCase, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
 
 class Context(models.Model):
     name = models.CharField(max_length=200)
     short_description = models.CharField(max_length=1000)
     long_description = models.CharField(max_length=3000)
     shape = models.IntegerField(default=0)
+    created_date = models.DateTimeField(auto_now_add=True)
     goal_id = models.ForeignKey(TopLevelNormativeGoal, on_delete=models.CASCADE)
 
 class SystemDescription(models.Model):
