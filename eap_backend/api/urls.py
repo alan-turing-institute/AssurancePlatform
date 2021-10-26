623e9d1c4f87f549114c7fd216e829bb691d0970
nbarlowATI <nbarlow@turing.ac.uk>
2021-10-26 16:40:18 +0100

    add new endpoints to urls.py

--- eap_backend/api/urls.py
+++ eap_backend/api/urls.py
@@ -14,5 +14,17 @@ urlpatterns = [
     path('cases/<int:pk>/', views.case_detail),
     path('goals/', views.goal_list),
     path('goals/<int:pk>/', views.goal_detail),
+    path('contexts/', views.context_list),
+    path('contexts/<int:pk>/', views.context_detail),
+    path('descriptions/', views.description_list),
+    path('descriptions/<int:pk>/', views.description_detail),
+    path('propertyclaims/', views.property_claim_list),
+    path('propertyclaims/<int:pk>/', views.property_claim_detail),
+    path('arguments/', views.argument_list),
+    path('arguments/<int:pk>/', views.argument_detail),
+    path('evidentialclaims/', views.evidential_claim_list),
+    path('evidentialclaims/<int:pk>/', views.evidential_claim_detail),
+    path('evidence/', views.evidence_list),
+    path('evidence/<int:pk>/', views.evidence_detail),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
