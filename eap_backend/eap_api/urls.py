9c401b292da2fe2b656c1f5a70502810afb42a69
nbarlowATI <nbarlow@turing.ac.uk>
2021-11-09 17:46:03 +0000

    add other model classes into views and urls

--- eap_backend/eap_api/urls.py
+++ eap_backend/eap_api/urls.py
@@ -3,28 +3,22 @@ from django.contrib.auth.models import User
 from rest_framework import routers
 from . import views
 
-# Routers provide an easy way of automatically determining the URL conf.
-#router = routers.DefaultRouter()
-#router.register(r'cases', views.AssuranceCaseViewSet)
-#router.register(r'goals', views.TopLevelNormativeGoalViewSet)
-
-
 urlpatterns = [
     path('cases/', views.case_list, name='case_list'),
     path('cases/<int:pk>/', views.case_detail, name='case_detail'),
     path('goals/', views.goal_list, name='goals'),
     path('goals/<int:pk>/', views.goal_detail, name='goals/'),
-    #path('contexts/', views.context_list),
-    #path('contexts/<int:pk>/', views.context_detail),
-    #path('descriptions/', views.description_list),
-    #path('descriptions/<int:pk>/', views.description_detail),
-    #path('propertyclaims/', views.property_claim_list),
-    #path('propertyclaims/<int:pk>/', views.property_claim_detail),
-    #path('arguments/', views.argument_list),
-    #path('arguments/<int:pk>/', views.argument_detail),
-    #path('evidentialclaims/', views.evidential_claim_list),
-    #path('evidentialclaims/<int:pk>/', views.evidential_claim_detail),
-    #path('evidence/', views.evidence_list),
-    #path('evidence/<int:pk>/', views.evidence_detail),
-    #path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('contexts/', views.context_list),
+    path('contexts/<int:pk>/', views.context_detail),
+    path('descriptions/', views.description_list),
+    path('descriptions/<int:pk>/', views.description_detail),
+    path('propertyclaims/', views.property_claim_list),
+    path('propertyclaims/<int:pk>/', views.property_claim_detail),
+    path('arguments/', views.argument_list),
+    path('arguments/<int:pk>/', views.argument_detail),
+    path('evidentialclaims/', views.evidential_claim_list),
+    path('evidentialclaims/<int:pk>/', views.evidential_claim_detail),
+    path('evidence/', views.evidence_list),
+    path('evidence/<int:pk>/', views.evidence_detail),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- eap_backend/eap_api/views.py
+++ eap_backend/eap_api/views.py
@@ -53,7 +53,7 @@ def case_detail(request, pk):
 @csrf_exempt
 def goal_list(request):
     """
-    List all goals, or make a new case
+    List all goals, or make a new goal
     """
     if request.method == "GET":
         goals = TopLevelNormativeGoal.objects.all()
@@ -89,4 +89,255 @@ def goal_detail(request, pk):
         return JsonResponse(serializer.errors, status=400)
     elif request.method == "DELETE":
         goal.delete()
+        return HttpResponse(status=204)
+
+
+@csrf_exempt
+def context_list(request):
+    """
+    List all contexts, or make a new context
+    """
+    if request.method == "GET":
+        contexts = Context.objects.all()
+        serializer = ContextSerializer(contexts, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = ContextSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def context_detail(request, pk):
+    """
+    Retrieve, update, or delete a Context, by primary key
+    """
+    try:
+        context = Context.objects.get(pk=pk)
+    except Context.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == "GET":
+        serializer = ContextSerializer(context)
+        return JsonResponse(serializer.data)
+    elif request.method == "PUT":
+        data = JSONParser().parse(request)
+        serializer = ContextSerializer(context, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == "DELETE":
+        context.delete()
+        return HttpResponse(status=204)
+
+
+@csrf_exempt
+def description_list(request):
+    """
+    List all descriptions, or make a new description
+    """
+    if request.method == "GET":
+        descriptions = SystemDescription.objects.all()
+        serializer = SystemDescriptionSerializer(descriptions, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = SystemDescriptionSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def description_detail(request, pk):
+    """
+    Retrieve, update, or delete a SystemDescription, by primary key
+    """
+    try:
+        description = SystemDescription.objects.get(pk=pk)
+    except SystemDescription.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == "GET":
+        serializer = SystemDescriptionSerializer(description)
+        return JsonResponse(serializer.data)
+    elif request.method == "PUT":
+        data = JSONParser().parse(request)
+        serializer = SystemDescriptionSerializer(description, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == "DELETE":
+        description.delete()
+        return HttpResponse(status=204)
+
+@csrf_exempt
+def property_claim_list(request):
+    """
+    List all claims, or make a new claim
+    """
+    if request.method == "GET":
+        claims = PropertyClaim.objects.all()
+        serializer = PropertyClaimSerializer(claims, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = PropertyClaimSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def property_claim_detail(request, pk):
+    """
+    Retrieve, update, or delete a PropertyClaim, by primary key
+    """
+    try:
+        claim = PropertyClaim.objects.get(pk=pk)
+    except PropertyClaim.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == "GET":
+        serializer = PropertyClaimSerializer(claim)
+        return JsonResponse(serializer.data)
+    elif request.method == "PUT":
+        data = JSONParser().parse(request)
+        serializer = PropertyClaimSerializer(claim, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == "DELETE":
+        claim.delete()
+        return HttpResponse(status=204)
+
+
+@csrf_exempt
+def argument_list(request):
+    """
+    List all arguments, or make a new argument
+    """
+    if request.method == "GET":
+        arguments = Argument.objects.all()
+        serializer = ArgumentSerializer(arguments, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = ArgumentSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def argument_detail(request, pk):
+    """
+    Retrieve, update, or delete a Argument, by primary key
+    """
+    try:
+        argument = Argument.objects.get(pk=pk)
+    except Argument.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == "GET":
+        serializer = ArgumentSerializer(argument)
+        return JsonResponse(serializer.data)
+    elif request.method == "PUT":
+        data = JSONParser().parse(request)
+        serializer = ArgumentSerializer(argument, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == "DELETE":
+        argument.delete()
+        return HttpResponse(status=204)
+
+
+@csrf_exempt
+def evidential_claim_list(request):
+    """
+    List all evidential_claims, or make a new evidential_claim
+    """
+    if request.method == "GET":
+        evidential_claims = EvidentialClaim.objects.all()
+        serializer = EvidentialClaimSerializer(evidential_claims, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = EvidentialClaimSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def evidential_claim_detail(request, pk):
+    """
+    Retrieve, update, or delete a EvidentialClaim, by primary key
+    """
+    try:
+        evidential_claim = EvidentialClaim.objects.get(pk=pk)
+    except EvidentialClaim.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == "GET":
+        serializer = EvidentialClaimSerializer(evidential_claim)
+        return JsonResponse(serializer.data)
+    elif request.method == "PUT":
+        data = JSONParser().parse(request)
+        serializer = EvidentialClaimSerializer(evidential_claim, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == "DELETE":
+        evidential_claim.delete()
+        return HttpResponse(status=204)
+
+
+@csrf_exempt
+def evidence_list(request):
+    """
+    List all evidences, or make a new evidence
+    """
+    if request.method == "GET":
+        evidences = Evidence.objects.all()
+        serializer = EvidenceSerializer(evidences, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = EvidenceSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def evidence_detail(request, pk):
+    """
+    Retrieve, update, or delete a Evidence, by primary key
+    """
+    try:
+        evidence = Evidence.objects.get(pk=pk)
+    except Evidence.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == "GET":
+        serializer = EvidenceSerializer(evidence)
+        return JsonResponse(serializer.data)
+    elif request.method == "PUT":
+        data = JSONParser().parse(request)
+        serializer = EvidenceSerializer(evidence, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == "DELETE":
+        evidence.delete()
         return HttpResponse(status=204)
