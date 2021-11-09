355cb0f9b27ab210f61d7e994ef3d452a24fea0e
nbarlowATI <nbarlow@turing.ac.uk>
2021-11-09 18:13:39 +0000

    add Shape class, and make the shape property of all our models into a fixed variable rather than a db field.

--- eap_backend/eap_api/models.py
+++ eap_backend/eap_api/models.py
@@ -1,14 +1,25 @@
 from django.db import models
 from django.utils import timezone
 import datetime
+from enum import Enum
 
 # Create your models here.
+
+class Shape(Enum):
+    """
+    Enum class to hold the various shapes for the objects on
+    our diagram.
+    """
+    RECTANGLE = 0
+    DIAMOND = 1
+    ROUNDED_RECTANGLE = 2
+    CYLINDER = 3
 
 class AssuranceCase(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=1000)
-    shape = models.IntegerField(default=0)
     created_date = models.DateTimeField(auto_now_add=True)
+    shape = None
     def __str__(self):
         return self.name
     def was_published_recently(self):
@@ -19,8 +30,8 @@ class TopLevelNormativeGoal(models.Model):
     short_description = models.CharField(max_length=1000)
     long_description = models.CharField(max_length=3000)
     keywords = models.CharField(max_length=3000)
-    shape = models.IntegerField(default=0)
     assurance_case = models.ForeignKey(AssuranceCase, on_delete=models.CASCADE)
+    shape = Shape.RECTANGLE
     def __str__(self):
         return self.name
 
@@ -28,7 +39,7 @@ class Context(models.Model):
     name = models.CharField(max_length=200)
     short_description = models.CharField(max_length=1000)
     long_description = models.CharField(max_length=3000)
-    shape = models.IntegerField(default=0)
+    shape = Shape.DIAMOND
     created_date = models.DateTimeField(auto_now_add=True)
     goal_id = models.ForeignKey(TopLevelNormativeGoal, on_delete=models.CASCADE)
 
@@ -36,28 +47,28 @@ class SystemDescription(models.Model):
     name = models.CharField(max_length=200)
     short_description = models.CharField(max_length=1000)
     long_description = models.CharField(max_length=3000)
-    shape = models.IntegerField(default=0)
+    shape = Shape.DIAMOND
     goal_id = models.ForeignKey(TopLevelNormativeGoal, on_delete=models.CASCADE)
 
 class PropertyClaim(models.Model):
     name = models.CharField(max_length=200)
     short_description = models.CharField(max_length=1000)
     long_description = models.CharField(max_length=3000)
-    shape = models.IntegerField(default=0)
+    shape = Shape.ROUNDED_RECTANGLE
     goal_id = models.ForeignKey(TopLevelNormativeGoal, on_delete=models.CASCADE)
 
 class Argument(models.Model):
     name = models.CharField(max_length=200)
     short_description = models.CharField(max_length=1000)
     long_description = models.CharField(max_length=3000)
-    shape = models.IntegerField(default=0)
+    shape = Shape.ROUNDED_RECTANGLE
     property_claim_id =  models.ManyToManyField(PropertyClaim)
 
 class EvidentialClaim(models.Model):
     name = models.CharField(max_length=200)
     short_description = models.CharField(max_length=1000)
     long_description = models.CharField(max_length=3000)
-    shape = models.IntegerField(default=0)
+    shape = Shape.ROUNDED_RECTANGLE
     argument_id = models.ForeignKey(Argument, on_delete=models.CASCADE)
 
 class Evidence(models.Model):
@@ -65,5 +76,5 @@ class Evidence(models.Model):
     short_description = models.CharField(max_length=1000)
     long_description = models.CharField(max_length=3000)
     URL = models.CharField(max_length=3000)
-    shape = models.IntegerField(default=0)
+    shape = Shape.CYLINDER
     evidential_claim_id = models.ManyToManyField(EvidentialClaim)
