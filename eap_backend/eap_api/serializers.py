f7e13209692a9aba952ddc3f5ba5ac8505583298
nbarlowATI <nbarlow@turing.ac.uk>
2021-11-12 14:44:42 +0000

    add relations to serializers

--- eap_backend/eap_api/serializers.py
+++ eap_backend/eap_api/serializers.py
@@ -21,8 +21,8 @@ class AssuranceCaseSerializer(serializers.ModelSerializer):
 class TopLevelNormativeGoalSerializer(serializers.ModelSerializer):
     assurance_case = AssuranceCaseSerializer(many=False, read_only=True)
     assurance_case_id = serializers.PrimaryKeyRelatedField(source="assurance_case",queryset=AssuranceCase.objects.all(),write_only=True)
-#    context = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
-#    system_description = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
+    context = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    system_description = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     class Meta:
         model = TopLevelNormativeGoal
         fields = [
@@ -33,8 +33,8 @@ class TopLevelNormativeGoalSerializer(serializers.ModelSerializer):
             "keywords",
             "assurance_case",
             "assurance_case_id",
-#            "context",
-#            "system_description"
+            "context",
+            "system_description"
         ]
 
 
@@ -70,22 +70,88 @@ class SystemDescriptionSerializer(serializers.ModelSerializer):
 
 
 class PropertyClaimSerializer(serializers.ModelSerializer):
+    goal = TopLevelNormativeGoalSerializer(many=False, read_only=True)
+    goal_id = serializers.PrimaryKeyRelatedField(
+        source="goal",
+        queryset=TopLevelNormativeGoal.objects.all(),
+        write_only=True
+    )
+    arguments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     class Meta:
         model = PropertyClaim
-        fields = ["name", "short_description", "long_description"]
+        fields = [
+            "id",
+            "name",
+            "short_description",
+            "long_description",
+            "goal",
+            "goal_id",
+            "arguments"
+        ]
 
 class ArgumentSerializer(serializers.ModelSerializer):
+    property_claim = PropertyClaimSerializer(many=True, read_only=True)
+    property_claim_id = serializers.PrimaryKeyRelatedField(
+        source="property_claim",
+        queryset = PropertyClaim.objects.all(),
+        write_only=True,
+        many=True
+    )
+    evidential_claims = serializers.PrimaryKeyRelatedField(
+        many=True, read_only=True
+    )
     class Meta:
         model = Argument
-        fields = ["name", "short_description", "long_description"]
+        fields = [
+            "id",
+            "name",
+            "short_description",
+            "long_description",
+            "property_claim",
+            "property_claim_id",
+            "evidential_claims"
+        ]
 
 class EvidentialClaimSerializer(serializers.ModelSerializer):
+    argument = ArgumentSerializer(many=False, read_only=True)
+    argument_id = serializers.PrimaryKeyRelatedField(
+        source="argument",
+        queryset=Argument.objects.all(),
+        write_only=True,
+        many=False
+    )
+    evidence = serializers.PrimaryKeyRelatedField(
+        many=True, read_only=True
+    )
     class Meta:
         model = EvidentialClaim
-        fields = ["name", "short_description", "long_description"]
+        fields = [
+            "id",
+            "name",
+            "short_description",
+            "long_description",
+            "argument",
+            "argument_id",
+            "evidence"
+        ]
 
 
 class EvidenceSerializer(serializers.ModelSerializer):
+    evidential_claim = EvidentialClaimSerializer(many=True, read_only=True)
+    evidential_claim_id = serializers.PrimaryKeyRelatedField(
+        source="evidential_claim",
+        queryset=EvidentialClaim.objects.all(),
+        write_only=True,
+        many=True
+    )
     class Meta:
         model = Evidence
-        fields = ["name", "short_description", "long_description", "URL"]
+        fields = [
+            "id",
+            "name",
+            "short_description",
+            "long_description",
+            "URL",
+            "evidential_claim",
+            "evidential_claim_id"
+        ]
