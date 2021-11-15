ddfa41f71a5577f799e20e405fb7b7366eed0b60
nbarlowATI <nbarlow@turing.ac.uk>
2021-11-15 18:06:10 +0000

    add any ALLOWED_HOST, for testing web deployment

--- eap_backend/eap_backend/settings.py
+++ eap_backend/eap_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-)@nls8m9den@jbfjkee2h343^=a8#jzq+@^nweds$s#%_1ia_g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
