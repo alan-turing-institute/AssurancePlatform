e64d219c8d4cc7ea329e5d1e541284edbdb58fd1
nbarlowATI <nbarlow@turing.ac.uk>
2021-11-16 14:52:05 +0000

    use sqlite db for tests

--- eap_backend/eap_backend/settings.py
+++ eap_backend/eap_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -110,7 +111,10 @@ DATABASES = {
 
     }
 }
-
+## use sqlite db for tests
+if 'test' in sys.argv or 'test_coverage' in sys.argv: #Covers regular testing and django-coverage
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+    DATABASES['default']["NAME"] = BASE_DIR / "db.sqlite3"
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
