48eaee057e2f8652589df28ac3deac0254ef929d
nbarlowATI <nbarlow@turing.ac.uk>
2022-02-03 15:20:00 +0000

    add navigation bar

--- frontend/src/components/Routes.js
+++ frontend/src/components/Routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
+import Navigation from './Navigation.js';
 import Home from './Home.js';
 import CaseCreator from './CaseCreator.js'
 import CaseSelector from './CaseSelector.js'
 import CaseContainer from './CaseContainer.js';
 const AllRoutes = () => (
     <Router>
+        <Navigation />
         <Routes>
             <Route exact path="/" element={<Home/>} />
             <Route path="/case/new" element={<CaseCreator/>} />
