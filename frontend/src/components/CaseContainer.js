2519eec078107c771cf3ed62891c3bd93973ac98
nbarlowATI <nbarlow@turing.ac.uk>
2022-02-16 18:32:15 +0000

    fix rendering of graph when reading from backend json

--- frontend/src/components/CaseContainer.js
+++ frontend/src/components/CaseContainer.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
 import { useParams } from "react-router-dom";
 import { Grid, Box, DropButton, Menu, TextInput, Layer, Button } from 'grommet';
-import { grommet } from 'grommet/themes';
-
-import CaseDetails from './CaseDetails.js'
 import MermaidChart from './mermaid';
 import configData from "../config.json";
 
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
-var input_json = { "id": 1, "name": "My Case", "description": "first test case", "created_date": "2021-11-15T17:52:41.675611Z", "goals": [{ "id": 1, "name": "The Goal", "short_description": "we should do this", "long_description": "A long description of what we should do", "keywords": "key", "assurance_case": 1, "context": [{ "id": 1, "name": "Context", "short_description": "context for The Goal", "long_description": "A longer description of the context", "created_date": "2021-11-15T17:52:41.698240Z", "goal": 1 }], "system_description": [{ "id": 1, "name": "Description", "short_description": "A short description of the system", "long_description": "a much longer description of the system", "goal": 1 }], "property_claims": [{ "id": 1, "name": "PropertyClaim 1", "short_description": "Goal 1 should be x", "long_description": "A long description of this property claim", "goal": 1, "arguments": [{ "id": 1, "name": "Argument 1", "short_description": "This is an argument", "long_description": "looong description of the argument", "property_claim": [1], "evidential_claims": [{ "id": 1, "name": "Evidential Claim 1", "short_description": "A short description of the first evidential claim", "long_description": "A longer description of the first evidential claim", "argument": 1, "evidence": [{ "id": 1, "name": "Evidence 1", "short_description": "Some evidence to support claim 1", "long_description": "Description of that evidence", "URL": "http://evidence1.com", "evidential_claim": [1] }, { "id": 2, "name": "Evidence 2", "short_description": "Some more evidence to support claim 1", "long_description": "Description of that evidence", "URL": "http://evidence2.com", "evidential_claim": [1] }] }] }, { "id": 2, "name": "Argument 2", "short_description": "This is also an argument", "long_description": "looong description of the second argument", "property_claim": [1], "evidential_claims": [{ "id": 2, "name": "Evidential Claim 2", "short_description": "A short description of the second evidential claim", "long_description": "A longer description of the second evidential claim", "argument": 2, "evidence": [{ "id": 3, "name": "Evidence 3", "short_description": "Some evidence to support claim 2", "long_description": "Description of that evidence", "URL": "http://evidence3.com", "evidential_claim": [2] }, { "id": 4, "name": "Evidence 4", "short_description": "Some more evidence to support claim 2", "long_description": "Description of that evidence", "URL": "http://evidence4.com", "evidential_claim": [2] }] }] }] }, { "id": 2, "name": "PropertyClaim 2", "short_description": "Goal 1 should probably be yz", "long_description": "A long description of this property claim", "goal": 1, "arguments": [{ "id": 3, "name": "Argument 3", "short_description": "Would you believe this is also an argument", "long_description": "looong description of the third argument", "property_claim": [2], "evidential_claims": [{ "id": 3, "name": "Evidential Claim 3", "short_description": "A short description of the third evidential claim", "long_description": "A longer description of the third evidential claim", "argument": 3, "evidence": [{ "id": 5, "name": "Evidence 5", "short_description": "Some evidence to support claim 3", "long_description": "Description of that evidence", "URL": "http://evidence5.com", "evidential_claim": [3] }, { "id": 6, "name": "Evidence 6", "short_description": "Some more evidence to support claim 3", "long_description": "Description of that evidence", "URL": "http://evidence6.com", "evidential_claim": [3] }] }, { "id": 4, "name": "Evidential Claim 4", "short_description": "A short description of the fourth evidential claim", "long_description": "A longer description of the fourth evidential claim", "argument": 3, "evidence": [{ "id": 7, "name": "Evidence 7", "short_description": "Some more evidence to support claim 4", "long_description": "Description of that evidence", "URL": "http://evidence7.com", "evidential_claim": [4] }] }] }] }] }] }
-
-
 class CaseContainer extends Component {
-  state = {
-    assurance_case: {
-      id: 0,
-      name: "",
-      description: ""
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: true,
+      assurance_case: {
+        id: 0,
+        name: "",
+        description: "",
+        goals: []
+      },
+      mermaid_md: "graph TB; \n"
     }
+
+    this.url = `${configData.BASE_URL}/cases/`;
+ 
+    console.log("In CaseContainer::constructor ");
   };
 
-  url = `${configData.BASE_URL}/cases/`;
 
   handleChange = async (id) => {
 
@@ -31,23 +34,25 @@ class CaseContainer extends Component {
 
   fetchData = async (id) => {
     const res = await fetch(this.url + id);
-    console.log(" did some fetching ", res)
     const json_response = await res.json()
 
     console.log(json_response);
     this.setState({
       assurance_case: json_response
     });
+    this.setState({
+      mermaid_md: this.jsonToMermaid(this.state.assurance_case)
+    })
+    this.setState({loading:false})
   }
 
   componentDidMount() {
     const id = this.props.params.caseSlug;
-    this.fetchData(id);
-    console.log("In CaseContainer::componentDidMount ", id);
+    this.fetchData(id);   
   }
 
-
-  jsontoMermaid(in_json) {
+  jsonToMermaid(in_json) {
+    console.log("in jsonToMermaid ", in_json)
     let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
     let letterIndex = 0;
     function getNextLetter() {
@@ -104,15 +109,18 @@ class CaseContainer extends Component {
       outputmd += goalLetter + squareBox(goal["name"])
       /// Add a box for the Context - only one per goal
       let contextLetter = getNextLetter();
-      outputmd += arrow + contextLetter + diamondBox(goal["context"][0]["name"]) +"\n"
+      if (goal["context"].length > 0 ) {
+        outputmd += arrow + contextLetter + diamondBox(goal["context"][0]["name"]) +"\n"
+      }
       /// now start the recursive process of adding PropertyClaims and descendents
       outputmd = addTree("property_claims", goal, goalLetter, outputmd)
       /// Add SystemDescription to the right of all the PropertyClaims and descendants
       let descriptionLetter = getNextLetter();
-      outputmd += goalLetter + arrow + descriptionLetter + diamondBox(goal["system_description"][0]["name"]) +"\n"
+      if (goal["system_description"].length > 0) {
+        outputmd += goalLetter + arrow + descriptionLetter + diamondBox(goal["system_description"][0]["name"]) +"\n"
+      }
     }
     outputmd += " \n"
-    //console.log("outputmd is ",outputmd)
 
     return (outputmd)
   }
@@ -135,7 +143,12 @@ class CaseContainer extends Component {
   }
 
   render() {
-    console.log(this.jsontoMermaid(input_json));
+    if (this.state.loading) {
+      return (
+        <div>loading</div>
+      )
+    } else {
+
     return (
       <div>
 
@@ -150,19 +163,10 @@ class CaseContainer extends Component {
             { name: 'footer', start: [0, 2], end: [1, 2] },
           ]}
         >
-          <Box gridArea="header" background="#ffffff" >
-            {/* <CaseDetails acase={this.state.assurance_case} /> */}
-          </Box>
-          {/* <Box gridArea="title" background="light-2" >
-            <h2>{input_json.name}</h2>
-          </Box> */}
-          {/* <div class="flex flex-wrap">
-
-            <div class="w-25pc h-1by1 pattern-dots-md slategray-lighter"></div>
-          </div> */}
+    
           <Box gridArea="main" background={{ color: "white", size: "20px 20px", image: "radial-gradient(#999999 0.2%, transparent 10%)", height: "200px", width: "100%", repeat: "repeat-xy" }}>
             {/* {this.Example()} */}
-            <Box width={"flex"} height={'30px'} >  <h2> &nbsp;{input_json.name}</h2>  </Box>
+            <Box width={"flex"} height={'30px'} >  <h2> &nbsp;{this.state.assurance_case.name}</h2>  </Box>
             <TransformWrapper
               initialScale={1}
               initialPositionX={25}
@@ -172,12 +176,7 @@ class CaseContainer extends Component {
                 <React.Fragment>
                   <TransformComponent >
                     <MermaidChart
-                      chartmd={this.jsontoMermaid(input_json)}
-                      goals={input_json.goals[0]["name"]}
-                      context={input_json.goals[0]["context"][0]["name"]}
-                      prop_claims={input_json.goals[0]["property_claims"][0]["name"]}
-                      syst_descr={input_json.goals[0]["system_description"][0]["name"]}
-                      assurance_name={input_json.name}
+                      chartmd={ this.state.mermaid_md}
                     />
                   </TransformComponent>
                   <div className="tools">
@@ -268,7 +267,8 @@ class CaseContainer extends Component {
         </Grid >
 
       </div >
-    );
+    )
+    }
   }
 }
 
