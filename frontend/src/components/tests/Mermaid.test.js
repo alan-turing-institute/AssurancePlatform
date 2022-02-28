c96de74183b88044a27ca03b58d0d5e4b5370726
nbarlowATI <nbarlow@turing.ac.uk>
2022-02-28 18:37:20 +0000

    test directory

---
+++ frontend/src/components/tests/Mermaid.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Mermaid from '../Mermaid.js';
+
+test('renders chart', () => {
+  render(<Mermaid />);
+ // const linkElement = screen.getByText(/Select an existing case/i);
+  //expect(linkElement).toBeInTheDocument();
+  expect(true);
+});
