bd77c30db1965911134823086ba0d7d63624546b
Markus Hauru <mhauru@turing.ac.uk>
2022-03-03 16:35:55 +0000

    Remove unnecessary nesting in Mermaid.js

--- frontend/src/components/Mermaid.js
+++ frontend/src/components/Mermaid.js
@@ -1,9 +1,7 @@
 import React from "react"
-//import Mermaid from "mermaid"
 import mermaid from "mermaid"
 
-
-class Mermaid extends React.Component {
+class MermaidChart extends React.Component {
 
   componentDidMount() {
     mermaid.initialize({
@@ -28,71 +26,10 @@ class Mermaid extends React.Component {
     window.callback = e => this.props.viewLayerFunc(e)
     mermaid.contentLoaded();
   }
-  render() {
-    return <div className="mermaid">{this.props.chart}</div>;
-  }
-}
-
-const keywords = [
-  "Goal",
-  "Context",
-  "System Description",
-  "Property Claim",
-  "Argument"
-];
-
-class MermaidChart extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      keywords: [],
-      keyphrases: []
-    };
-  }
-
-  componentDidMount() {
-
-    for (let kwrd of keywords) {
-      if (kwrd.indexOf(" ") >= 0) {
-        this.setState((prevState) => ({
-          keyphrases: [...prevState.keyphrases, kwrd]
-        }));
-      } else {
-        this.setState((prevState) => ({
-          keywords: [...prevState.keywords, kwrd]
-        }));
-      }
-    }
-  }
-  callback(message) {
-    console.log(message)
-    alert('For example you can display an alert!');
-  }
 
   render() {
-    return (
-      <div className="App" >
-        <Mermaid
-          viewLayerFunc={e => this.props.viewLayerFunc(e)}
-          chart={this.props.chartmd}
-        />
-      </div >
-    );
+    return <div className="mermaid">{this.props.chartmd}</div>
   }
 }
-
-
-//example mermaid graph:
-// `graph TB;
-// A[${this.state.keywords.length > 0 ? this.state.keywords[0] : "Goal"
-// }] --> B{Context}
-// A--> |keyword| E(<font color=white> System or Project Property Claim)
-// A--> D{System Description}
-// E--> F(Argument)
-// F--> G(Evidential Claim)
-// G--> |${Similarity}| H[(Evidence)]
-// style A fill:#f9f, stroke:#333, stroke-width:3px,  padding:250px
-// `
 
 export default MermaidChart;
-
