e8b2e6b42a4830c1ba6de7318bfd91ae381a4abf
nbarlowATI <nbarlow@turing.ac.uk>
2022-03-16 10:10:48 +0000

    prettier linting

--- frontend/src/components/App.test.js
+++ frontend/src/components/App.test.js
@@ -1,11 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Home from './Home.js';
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import { Link } from "react-router-dom";
+import Home from "./Home.js";
 
-test('renders homepage', () => {
+test("renders homepage", () => {
   //render(<Home />);
- // const linkElement = screen.getByText(/Select an existing case/i);
+  // const linkElement = screen.getByText(/Select an existing case/i);
   //expect(linkElement).toBeInTheDocument();
   expect(true);
 });
