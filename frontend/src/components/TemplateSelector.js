829369a8ca19d668709f902df58b58e8f9eb6f7e
Markus Hauru <mhauru@turing.ac.uk>
2022-03-31 18:07:13 +0100

    Fix two bugs in CaseCreator
    * Send the description to the backend
    * Use the Empty template by default

--- frontend/src/components/TemplateSelector.js
+++ frontend/src/components/TemplateSelector.js
@@ -1,25 +1,44 @@
 import { Select } from "grommet";
-import React from "react";
+import React, { Component } from "react";
+import { useParams } from "react-router-dom";
 
-function TemplateSelector(props) {
-  // Import all *.json files from caseTemplates, make a list of the contents.
-  const templates = [];
-  const rc = require.context("../caseTemplates", false, /.json$/);
-  rc.keys().forEach((key) => templates.push(rc(key)));
-
-  function onChange(event) {
-    props.setValue(event.value);
+class TemplateSelector extends Component {
+  constructor(props) {
+    super(props);
+    this.templates = [];
   }
 
-  return (
-    <Select
-      placeholder="Choose template"
-      onChange={onChange}
-      value={props.value}
-      options={templates}
-      labelKey="name"
-    />
-  );
+  componentDidMount() {
+    // Import all *.json files from caseTemplates, make a list of the contents.
+    const rc = require.context("../caseTemplates", false, /.json$/);
+    rc.keys().forEach((key) => this.templates.push(rc(key)));
+    // The default case is the one called empty, if it exists, otherwise the first one.
+    if (this.templates.length === 0) return null;
+    let defaultCase = this.templates[0];
+    for (let c of this.templates) {
+      if (c.name === "empty") {
+        defaultCase = c;
+        break;
+      }
+    }
+    this.props.setValue(defaultCase);
+  }
+
+  onChange(event) {
+    this.props.setValue(event.value);
+  }
+
+  render() {
+    return (
+      <Select
+        placeholder="Choose template"
+        onChange={this.onChange.bind(this)}
+        value={this.props.value}
+        options={this.templates}
+        labelKey="name"
+      />
+    );
+  }
 }
 
-export default TemplateSelector;
+export default (props) => <TemplateSelector {...props} params={useParams()} />;
