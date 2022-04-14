42fb333749c47c501f9199ccde5d94a3762d6863
nbarlowATI <nbarlow@turing.ac.uk>
2022-04-14 16:49:35 +0100

    use padding with spaces to (almost) get same-width mermaid boxes, and add skeleton CSS

--- frontend/src/components/utils.js
+++ frontend/src/components/utils.js
@@ -17,15 +17,15 @@ function jsonToMermaid(in_json) {
   }
 
   function makeBox(text, shape) {
-    if (text.length > configData["BOX_WIDTH"]) {
-      text = text.substring(0, configData["BOX_WIDTH"] - 3) + "...";
+    if (text.length > configData["BOX_NCHAR"]) {
+      text = text.substring(0, configData["BOX_NCHAR"] - 3) + "...";
     } else {
       // pad the text with spaces to make it the same width
-      let nSpaces = configData["BOX_WIDTH"] - text.length;
+      let nSpaces = configData["BOX_NCHAR"] - text.length;
       text =
-        "&#160".repeat(Math.floor(nSpaces / 2)) +
+        "&#160".repeat(Math.ceil(nSpaces / 2)) +
         text +
-        "&#160".repeat(Math.ceil(nSpaces / 2));
+        "&#160".repeat(Math.floor(nSpaces / 2));
     }
     if (shape === "square") return "[" + text + "]";
     else if (shape === "diamond") return "{" + text + "}";
@@ -63,6 +63,8 @@ function jsonToMermaid(in_json) {
         ' callback "' +
         thisObj.short_description +
         '"\n';
+      // add style to the node
+      outputmd += "\nclass " + thisNode + " blackBox;\n";
       for (
         let j = 0;
         j < configData.navigation[itemType]["children"].length;
@@ -72,11 +74,13 @@ function jsonToMermaid(in_json) {
         outputmd = addTree(childType, thisObj, thisNode, outputmd);
       }
     }
-    //console.log(outputmd)
+    console.log(outputmd);
     return outputmd;
   }
 
   let outputmd = "graph TB; \n";
+  outputmd +=
+    "classDef blackBox stroke:#333,stroke-width:3px,text-align:center; \n";
   // call the recursive addTree function, starting with the Goal as the top node
   outputmd = addTree("TopLevelNormativeGoal", in_json, null, outputmd);
 
