c35fae92c158d39f8b5b87aa5e113325b11aee01
nbarlowATI <nbarlow@turing.ac.uk>
2022-05-09 14:45:32 +0100

    add homepage content - minimal styling

--- frontend/src/components/Routes.js
+++ frontend/src/components/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "../index.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Box, Grid, Text } from "grommet";
 import Navigation from "./Navigation.js";
@@ -9,34 +9,36 @@ import CaseSelector from "./CaseSelector.js";
 import CaseContainer from "./CaseContainer.js";
 const AllRoutes = () => (
   <Router>
-    <Grid
-      rows={["auto", "flex", "auto"]}
-      columns={["fill"]}
-      height="full"
-      gap="none"
-      areas={[
-        { name: "nav", start: [0, 0], end: [0, 0] },
-        { name: "main", start: [0, 1], end: [0, 1] },
-        { name: "footer", start: [0, 2], end: [0, 2] },
-      ]}
-    >
-      <Box gridArea="nav">
-        <Navigation />
-      </Box>
-      <Box gridArea="main">
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/case/new" element={<CaseCreator />} />
-          <Route path="/case/select" element={<CaseSelector />} />
-          <Route path="/case">
-            <Route path=":caseSlug" element={<CaseContainer />} />
-          </Route>
-        </Routes>
-      </Box>
-      <Box gridArea="footer" width="100%" background="dark-2" pad="small">
-        <Text>&copy; The Alan Turing Institute</Text>
-      </Box>
-    </Grid>
+    <Box full={true}>
+      <Grid
+        rows={["auto", "flex", "auto"]}
+        columns={["fill"]}
+        height="full"
+        gap="none"
+        areas={[
+          { name: "nav", start: [0, 0], end: [0, 0] },
+          { name: "main", start: [0, 1], end: [0, 1] },
+          { name: "footer", start: [0, 2], end: [0, 2] },
+        ]}
+      >
+        <Box gridArea="nav">
+          <Navigation />
+        </Box>
+        <Box gridArea="main">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/case/new" element={<CaseCreator />} />
+            <Route path="/case/select" element={<CaseSelector />} />
+            <Route path="/case">
+              <Route path=":caseSlug" element={<CaseContainer />} />
+            </Route>
+          </Routes>
+        </Box>
+        <Box gridArea="footer" width="100%" background="dark-2" pad="small">
+          <Text>&copy; The Alan Turing Institute</Text>
+        </Box>
+      </Grid>
+    </Box>
   </Router>
 );
 
