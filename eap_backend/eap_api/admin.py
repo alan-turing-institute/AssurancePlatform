12ea428279bddbc4d30843ff0fc913be93769d5d
Markus Hauru <mhauru@turing.ac.uk>
2022-05-12 13:20:07 +0100

    More style fixing

--- eap_backend/eap_api/admin.py
+++ eap_backend/eap_api/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from .models import (
     AssuranceCase,
--- eap_backend/eap_api/forms.py
+++ eap_backend/eap_api/forms.py
@@ -1,4 +1,3 @@
-from django import forms
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from .models import EAPUser
 
