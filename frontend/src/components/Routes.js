8584056016250b1b2290e10611769fc313799ec8
nbarlowATI <nbarlow@turing.ac.uk>
2022-05-12 15:07:02 +0100

    remove wrapping box such that footer reverts to previous behaviour

--- frontend/src/components/Routes.js
+++ frontend/src/components/Routes.js
@@ -9,36 +9,34 @@ import CaseSelector from "./CaseSelector.js";
 import CaseContainer from "./CaseContainer.js";
 const AllRoutes = () => (
   <Router>
-    <Box full={true}>
-      <Grid
-        rows={["auto", "flex", "auto"]}
-        columns={["fill"]}
-        height="full"
-        gap="none"
-        areas={[
-          { name: "nav", start: [0, 0], end: [0, 0] },
-          { name: "main", start: [0, 1], end: [0, 1] },
-          { name: "footer", start: [0, 2], end: [0, 2] },
-        ]}
-      >
-        <Box gridArea="nav">
-          <Navigation />
-        </Box>
-        <Box gridArea="main">
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/case/new" element={<CaseCreator />} />
-            <Route path="/case/select" element={<CaseSelector />} />
-            <Route path="/case">
-              <Route path=":caseSlug" element={<CaseContainer />} />
-            </Route>
-          </Routes>
-        </Box>
-        <Box gridArea="footer" width="100%" background="dark-2" pad="small">
-          <Text>&copy; The Alan Turing Institute</Text>
-        </Box>
-      </Grid>
-    </Box>
+    <Grid
+      rows={["auto", "flex", "auto"]}
+      columns={["fill"]}
+      height="full"
+      gap="none"
+      areas={[
+        { name: "nav", start: [0, 0], end: [0, 0] },
+        { name: "main", start: [0, 1], end: [0, 1] },
+        { name: "footer", start: [0, 2], end: [0, 2] },
+      ]}
+    >
+      <Box gridArea="nav">
+        <Navigation />
+      </Box>
+      <Box gridArea="main">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/case/new" element={<CaseCreator />} />
+          <Route path="/case/select" element={<CaseSelector />} />
+          <Route path="/case">
+            <Route path=":caseSlug" element={<CaseContainer />} />
+          </Route>
+        </Routes>
+      </Box>
+      <Box gridArea="footer" width="100%" background="dark-2" pad="small">
+        <Text>&copy; The Alan Turing Institute</Text>
+      </Box>
+    </Grid>
   </Router>
 );
 
