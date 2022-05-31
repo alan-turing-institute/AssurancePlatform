9f50452ab307827f40bc40e0a8de398d35ccfcd7
nbarlowATI <nbarlow@turing.ac.uk>
2022-05-31 16:08:55 +0100

    add new frontend tests

---
+++ frontend/src/components/tests/Login.test.js
@@ -0,0 +1,11 @@
+import "regenerator-runtime/runtime";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import React from "react";
+import Login from "../Login.js";
+
+test("renders login component", () => {
+  render(<Login />);
+  const text = screen.getByText("Not already registered?");
+  expect(text).toBeInTheDocument();
+});
---
+++ frontend/src/components/tests/Logout.test.js
@@ -0,0 +1,17 @@
+import "regenerator-runtime/runtime";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import React from "react";
+import Logout from "../Logout.js";
+
+const mockedUsedNavigate = jest.fn();
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedUsedNavigate,
+}));
+
+test("renders logout component", () => {
+  render(<Logout />);
+  const text = screen.getByText("Are you sure you want to logout?");
+  expect(text).toBeInTheDocument();
+});
