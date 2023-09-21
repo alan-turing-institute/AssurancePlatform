4c7481f431eb28c1c072eaa7cb211264e2b1009d
AoifeHughes <ahughes@turing.ac.uk>
2023-09-21 10:53:32 +0100

    Added initial files

---
+++ frontend/src/components/WorkInProgressBanner.js
@@ -0,0 +1,14 @@
+// WorkInProcessBanner.js
+
+import React from 'react';
+import '../WorkInProcessBanner.css';
+
+const WorkInProcessBanner = () => {
+  return (
+    <div className="wip-banner">
+      🚧 This website is a work in process. 🚧
+    </div>
+  );
+}
+
+export default WorkInProcessBanner;
