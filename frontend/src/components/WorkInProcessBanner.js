55025e41801d704bf534b26c8d6370d23bf7a694
AoifeHughes <ahughes@turing.ac.uk>
2023-09-21 14:27:14 +0100

    Pre-commit fixes

---
+++ frontend/src/components/WorkInProcessBanner.js
@@ -0,0 +1,12 @@
+// WorkInProcessBanner.js
+
+import React from "react";
+import "./WorkInProcessBanner.css";
+
+const WorkInProcessBanner = () => {
+  return (
+    <div className="wip-banner">🚧 This website is a work in process. 🚧</div>
+  );
+};
+
+export default WorkInProcessBanner;
