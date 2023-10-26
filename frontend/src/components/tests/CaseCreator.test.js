1c214669312553b706a453fd9509f35fcd8bc260
AoifeHughes <ahughes@turing.ac.uk>
2023-10-26 19:40:59 +0100

    tests tweaked to fix

--- frontend/src/components/tests/CaseCreator.test.js
+++ frontend/src/components/tests/CaseCreator.test.js
@@ -6,10 +6,13 @@ import "@testing-library/jest-dom";
 import React from "react";
 import CaseCreator from "../CaseCreator.js";
 
-const mockedUsedNavigate = jest.fn();
+const mockedUseNavigate = jest.fn();
+const mockedUseLocation = jest.fn().mockReturnValue({ state: {} }); // return an object with the state property
+
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
-  useNavigate: () => mockedUsedNavigate,
+  useNavigate: () => mockedUseNavigate,
+  useLocation: () => mockedUseLocation,
 }));
 
 test("renders case creator layer", () => {
