ac0c291bb87dc45dea7cdb08f0ff8f2527455e93
AoifeHughes <ahughes@turing.ac.uk>
2023-11-06 23:06:22 +0000

    Add login state to Home component and dismissible banner component

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,96 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Image, Text, Grid, Button } from "grommet";
 import mockup_diagram from "../images/mockup-diagram.png";
 
-class Home extends React.Component {
-  getButtons() {
-    if (localStorage.getItem("token") != null) {
-      return (
-        <Box
-          gridArea="left"
-          direction="column"
-          gap="small"
-          pad={{
-            horizontal: "small",
-            top: "medium",
-            bottom: "large",
-          }}
-        >
-          <Button
-            href="/case/new"
-            justify="center"
-            fill={false}
-            label="Get started!"
-          />
-          <Button href="/logout" justify="center" fill={false} label="Logout" />
-        </Box>
-      );
-    } else {
-      return (
-        <Box
-          gridArea="left"
-          direction="column"
-          gap="small"
-          pad={{
-            horizontal: "small",
-            top: "medium",
-            bottom: "large",
-          }}
-        >
-          <Button href="/login" justify="center" fill={false} label="Login" />
-          <Button
-            href="/signup"
-            justify="center"
-            fill={false}
-            label="Sign up"
-          />
-        </Box>
-      );
-    }
-  }
+const Home = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  render() {
-    return (
-      <Box fill pad="medium" overflow="auto">
-        <Grid
-          fill
-          rows={["auto", "flex"]}
-          columns={["30%", "flex"]}
-          gap="none"
-          areas={[
-            { name: "main", start: [0, 1], end: [1, 1] },
-            { name: "left", start: [0, 0], end: [0, 1] },
-          ]}
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setIsLoggedIn(token != null);
+  }, []);
+
+  return (
+    <Box fill pad="medium" overflow="auto">
+      <Grid
+        fill
+        rows={["auto", "flex"]}
+        columns={["auto", "flex"]}
+        areas={[
+          { name: "header", start: [0, 0], end: [1, 0] },
+          { name: "sidebar", start: [0, 1], end: [0, 1] },
+          { name: "main", start: [1, 1], end: [1, 1] },
+        ]}
+        gap="none"
+      >
+        <Box
+          gridArea="header"
+          direction="row"
+          gap="small"
+          pad={{ horizontal: "small", vertical: "medium" }}
+          justify="between"
         >
-          <Box
-            gridArea="left"
-            direction="column"
-            gap="small"
-            pad={{
-              horizontal: "small",
-              top: "large",
-              bottom: "medium",
-            }}
-          >
-            <Text size="2xl">Ethical Assurance Platform</Text>
-            <Text size="medium">
-              A tool to support the responsible design, development, and
-              deployment of data-driven technologies.
-            </Text>
-            {this.getButtons()}
-          </Box>
-          <Box gridArea="main" justify="end">
-            <Image
-              fit="contain"
-              src={mockup_diagram}
-              alt="Ethical Assurance flowchart"
-              height="100%"
-            />
-          </Box>
-        </Grid>
-      </Box>
-    );
-  }
-}
+          <Text size="2xl">Ethical Assurance Platform</Text>
+        </Box>
+        <Box
+          gridArea="sidebar"
+          direction="column"
+          gap="small"
+          pad={{ horizontal: "small", top: "medium", bottom: "large" }}
+        >
+          {/* Sidebar content here */}
+        </Box>
+        <Box gridArea="main" justify="end">
+          <Image
+            fit="contain"
+            src={mockup_diagram}
+            alt="Ethical Assurance flowchart"
+          />
+        </Box>
+      </Grid>
+    </Box>
+  );
+};
 
 export default Home;
--- frontend/src/components/WorkInProcessBanner.js
+++ frontend/src/components/WorkInProcessBanner.js
@@ -1,9 +1,26 @@
-// WorkInProcessBanner.js
-
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./WorkInProcessBanner.css";
 
 const WorkInProcessBanner = () => {
+  // State to control whether the banner is shown or not
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    // Check sessionStorage to determine if the banner was previously dismissed
+    const isBannerDismissed =
+      sessionStorage.getItem("bannerDismissed") === "true";
+    setIsVisible(!isBannerDismissed);
+  }, []);
+
+  const dismissBanner = () => {
+    // When dismissing the banner, set the state and also update sessionStorage
+    setIsVisible(false);
+    sessionStorage.setItem("bannerDismissed", "true");
+  };
+
+  // Do not render the banner if it's not supposed to be visible
+  if (!isVisible) return null;
+
   return (
     <div className="wip-banner">
       🚧 This platform is undergoing significant changes. Please refer to our{" "}
@@ -15,6 +32,9 @@ const WorkInProcessBanner = () => {
         project README
       </a>{" "}
       for further information. 🚧
+      <button onClick={dismissBanner} className="dismiss-button">
+        X
+      </button>
     </div>
   );
 };
