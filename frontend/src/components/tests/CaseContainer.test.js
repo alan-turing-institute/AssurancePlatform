9e8e53634331d1292b61ef11f5184f428b580783
AoifeHughes <ahughes@turing.ac.uk>
2023-11-09 00:23:28 +0000

    Refactor CaseContainer.test.js for better readability and maintainability.

--- frontend/src/components/tests/CaseContainer.test.js
+++ frontend/src/components/tests/CaseContainer.test.js
@@ -2,26 +2,30 @@
  * @jest-environment jsdom
  */
 import { render, screen, waitFor } from "@testing-library/react";
-import "regenerator-runtime/runtime";
-import React from "react";
 import "@testing-library/jest-dom";
 import fetchMock from "jest-fetch-mock";
+import React from "react";
+import regeneratorRuntime from "regenerator-runtime";
+import CaseContainer from "../CaseContainer";
 
-import CaseContainer from "../CaseContainer.js";
+// Mock useParams and useNavigate
+const mockNavigate = jest.fn();
+const mockParams = jest.fn().mockReturnValue({ caseSlug: "1" });
 
-const mockedUsedNavigate = jest.fn();
 jest.mock("react-router-dom", () => ({
-  ...jest.requireActual("react-router-dom"),
-  useNavigate: () => mockedUsedNavigate,
+  ...jest.requireActual("react-router-dom"), // if you need other parts of the router besides navigate
+  useParams: () => mockParams(),
+  useNavigate: () => mockNavigate,
 }));
 
 fetchMock.enableMocks();
 
 beforeEach(() => {
   fetch.resetMocks();
+  localStorage.clear();
 });
-
-test("renders loading screen", () => {
+test("renders loading screen", async () => {
+  // Setup the mock response for the fetch call
   fetch.mockResponseOnce(
     JSON.stringify({
       id: 1,
@@ -30,26 +34,70 @@ test("renders loading screen", () => {
       goals: [],
       color_profile: "default",
     }),
-  );
+    { status: 200 },
+  ); // Ensure the status is set to 200 to simulate a successful fetch
+
   localStorage.setItem("token", "dummy");
-  render(<CaseContainer id="1" />);
-  const textElement = screen.getByText("loading");
+
+  const { unmount } = render(<CaseContainer />);
+
+  // Use findByText instead of getByText to wait for the element to appear
+  const textElement = await screen.findByText(/loading/i);
   expect(textElement).toBeInTheDocument();
+
+  // Additional debugging logs
+  console.log("Loading screen rendered");
+  console.log("LocalStorage token:", localStorage.getItem("token"));
+  console.log(
+    "Mocked fetch calls:",
+    fetch.mock.calls.map((call) => ({ url: call[0], options: call[1] })),
+  );
+
+  // Unmount the component to cleanup and avoid state update warnings
+  unmount();
 });
 
 test("renders case view", async () => {
-  fetch.mockResponseOnce(
-    JSON.stringify({
-      id: 1,
-      name: "Test case",
-      description: "",
-      goals: [],
-      color_profile: "default",
-    }),
+  // Setup the mock responses for all expected fetch calls
+  fetch.mockResponses(
+    [
+      JSON.stringify({
+        id: 1,
+        name: "Test case",
+        description: "",
+        goals: [],
+        color_profile: "default",
+      }),
+      { status: 200 },
+    ], // Mock response for case data
+    [
+      JSON.stringify({
+        /* user data */
+      }),
+      { status: 200 },
+    ], // Mock response for user data
+    [
+      JSON.stringify([
+        /* comments data */
+      ]),
+      { status: 200 },
+    ], // Mock response for comments
   );
+
   localStorage.setItem("token", "dummy");
-  render(<CaseContainer id="1" />);
-  await waitFor(() =>
-    expect(screen.getByDisplayValue("Test case")).toBeInTheDocument(),
+
+  const { unmount } = render(<CaseContainer />);
+
+  // Use findBy* queries instead of getBy* to wait for element to appear
+  const textElement = await screen.findByDisplayValue("Test case");
+  expect(textElement).toBeInTheDocument();
+
+  // Additional debugging logs
+  console.log(
+    "Mocked fetch calls:",
+    fetch.mock.calls.map((call) => ({ url: call[0], options: call[1] })),
   );
+
+  // Unmount the component to cleanup and avoid state update warnings
+  unmount();
 });
