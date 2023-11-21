eff86803eb4ec8b17599d63145d8728e1686c9da
AoifeHughes <ahughes@turing.ac.uk>
2023-11-21 13:38:58 +0000

    Update git commit and date in .env file

--- frontend/update_last_modified.sh
+++ frontend/update_last_modified.sh
@@ -1,20 +1,7 @@
-getDateSuffix() {
-  day=$1
-  if ((day == 1 || day == 21 || day == 31)); then
-    echo 'st'
-  elif ((day == 2 || day == 22)); then
-    echo 'nd'
-  elif ((day == 3 || day == 23)); then
-    echo 'rd'
-  else
-    echo 'th'
-  fi
-}
+# Get the current date in the format dd/mm/yy
+formatted_date=$(date +%d-%m-%y)
 
-day=$(date +%d | sed 's/^0*//') # removes leading zero
-suffix=$(getDateSuffix $day)
-formatted_date=$(git log -1 --date=format:"%A the %d$suffix of %B %Y" --format=%cd)
-
+# Writing to .env file
 echo GENERATE_SOURCEMAP=false > .env
 echo REACT_APP_GIT_COMMIT=$(git rev-parse HEAD) >> .env
 echo REACT_APP_GIT_COMMIT_DATE="$formatted_date" >> .env
