15f109be74fd5130c9105a2eb2bf9747deacf515
Richard Griffiths <rich.griffiths89@gmail.com>
2024-08-08 15:54:22 +0100

    Login with GitHub

--- next_frontend/types/next-auth.d.ts
+++ next_frontend/types/next-auth.d.ts
@@ -5,15 +5,18 @@ import 'next-auth/jwt';
 declare module 'next-auth' {
   interface Session {
     accessToken?: string;
+    provider?: string;
   }
 
   interface User {
     accessToken?: string;
+    provider?: string;
   }
 }
 
 declare module 'next-auth/jwt' {
   interface JWT {
     accessToken?: string;
+    provider?: string;
   }
 }
