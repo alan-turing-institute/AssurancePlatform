2e05bceb8c3a407242d871cf5a638b6dc2041556
Richard Griffiths <rich.griffiths89@gmail.com>
2024-08-14 18:22:08 +0100

    TEA logo added to dashboard

--- next_frontend/app/(main)/dashboard/layout.tsx
+++ next_frontend/app/(main)/dashboard/layout.tsx
@@ -196,12 +196,17 @@ export default function DashboardLayout({ children } : { children: React.ReactNo
         <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 dark:bg-slate-900 px-6 pb-4">
-            <div className="flex h-16 shrink-0 items-center">
+            <div className="flex shrink-0 items-center">
               <Link href={'/dashboard'}>
-                <p className='text-white font-semibold flex justify-start items-center gap-2'>
-                  <svg className='w-6 h-6' fill='white' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M9 2v2.06A8.522 8.522 0 0 0 4.05 9H2v6h2.06A8.494 8.494 0 0 0 9 19.95V22h6v-2.06A8.494 8.494 0 0 0 19.95 15H22V9h-2.06A8.522 8.522 0 0 0 15 4.05V2m-4 2h2v2h-2m-2 .25V8h6V6.25c1.18.61 2.14 1.57 2.75 2.75H16v6h1.75A6.406 6.406 0 0 1 15 17.75V16H9v1.75A6.406 6.406 0 0 1 6.25 15H8V9H6.25A6.406 6.406 0 0 1 9 6.25M4 11h2v2H4m14-2h2v2h-2m-7 5h2v2h-2"></path></svg>
-                  Assurance Platform
-                </p>
+                <div className='my-3 text-white font-semibold flex justify-start items-center gap-2'>
+                  {/* <svg className='w-6 h-6' fill='white' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M9 2v2.06A8.522 8.522 0 0 0 4.05 9H2v6h2.06A8.494 8.494 0 0 0 9 19.95V22h6v-2.06A8.494 8.494 0 0 0 19.95 15H22V9h-2.06A8.522 8.522 0 0 0 15 4.05V2m-4 2h2v2h-2m-2 .25V8h6V6.25c1.18.61 2.14 1.57 2.75 2.75H16v6h1.75A6.406 6.406 0 0 1 15 17.75V16H9v1.75A6.406 6.406 0 0 1 6.25 15H8V9H6.25A6.406 6.406 0 0 1 9 6.25M4 11h2v2H4m14-2h2v2h-2m-7 5h2v2h-2"></path></svg> */}
+                  <img 
+                    src='/images/tea-logo2.png' 
+                    alt='Turing Ethical Assurance Logo' 
+                    className='w-16'
+                  />
+                  <span className='text-sm leading-4 -ml-3'>Trustworthy and Ethical Assurance Platform</span>
+                </div>
               </Link>
             </div>
             <nav className="flex flex-1 flex-col">
