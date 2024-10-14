87e652b3d82c62fecb62d0994cf837dcac1128e3
Richard Griffiths <rich.griffiths89@gmail.com>
2024-10-14 09:50:34 +0100

    Missing storage account for prod

--- next_frontend/next.config.mjs
+++ next_frontend/next.config.mjs
@@ -9,6 +9,12 @@ const nextConfig = {
         port: "",
         pathname: "**/*",
       },
+      {
+        protocol: "https",
+        hostname: "teastorageaccount.blob.core.windows.net",
+        port: "",
+        pathname: "**/*",
+      },
     ],
   },
 };
