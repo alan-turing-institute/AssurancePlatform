ec6bf33ef4a75f533a2a19d3d9b198013666afe9
Richard Griffiths <rich.griffiths89@gmail.com>
2024-12-02 17:23:00 +0000

    Updated nav link

--- next_frontend/components/demo/landing/Hero.tsx
+++ next_frontend/components/demo/landing/Hero.tsx
@@ -6,7 +6,9 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { useLoginToken } from '@/hooks/useAuth'
 
 const navigation = [
-  { name: 'Documentation', href: 'https://alan-turing-institute.github.io/AssurancePlatform/' },
+  // { name: 'Showcase', href: 'documentation' },
+  { name: 'Documentation', href: 'documentation' },
+  // { name: 'Documentation', href: 'https://alan-turing-institute.github.io/AssurancePlatform/' },
   { name: 'GitHub', href: 'https://github.com/alan-turing-institute/AssurancePlatform' },
 ]
 
@@ -49,7 +51,7 @@ export default function Hero() {
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+              <a key={item.name} href={item.href} target='_blank' className="text-sm font-semibold leading-6 text-gray-900">
                 {item.name}
               </a>
             ))}
@@ -94,6 +96,7 @@ export default function Hero() {
                     <a
                       key={item.name}
                       href={item.href}
+                      target='_blank'
                       className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                     >
                       {item.name}
